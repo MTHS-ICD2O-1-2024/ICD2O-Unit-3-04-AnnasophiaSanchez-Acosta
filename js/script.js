88b20c17b3a4c0816d2e89f5343883fa2391bfd5
default <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-04-11 16:17:16 +0000

    .

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mr. Coxall updated by Anna S.
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates volume of a Sphere.
+ */
+function calculateVolumeOfSphere() {
+  // input
+  const theRadious = parseFloat(document.getElementById("radious").value)
+  // process
+  const volumeOfSphere=  (4 / 3) * (Math.PI) * (theRadious **3)
+
+  // output
+  document.getElementById("volume").innerHTML =
+    "Volume is: " + `${volumeOfSphere.toFixed(2)}` + " cm&sup3"
+}
